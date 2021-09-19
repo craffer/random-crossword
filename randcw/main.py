78edbaacd607541d99c57efa16e5d624a7dffe20
Conor Rafferty <craffer@umich.edu>
2021-09-19 13:33:29 -0700

    Add weekday logic

--- randcw/main.py
+++ randcw/main.py
@@ -13,13 +13,20 @@ def random_date(start_date, end_date):
     return start_date + datetime.timedelta(seconds=random_second)
 
 
+def random_weekday(start_date, end_date, weekday):
+    while True:
+        date = random_date(start_date, end_date)
+        if date.weekday() == weekday:
+            return date
+
+
 def date_to_nyt_url(date):
     return f"https://www.nytimes.com/crosswords/game/daily/{date.year}/{date.month}/{date.day}"
 
 
 def main():
-    print("Generated a random puzzle:")
-    print(date_to_nyt_url(random_date(NYT_ARCHIVE_START, TODAYS_DATE)))
+    print("Generated a random Monday puzzle:")
+    print(date_to_nyt_url(random_weekday(NYT_ARCHIVE_START, TODAYS_DATE, 0)))
 
 
 if __name__ == "__main__":
