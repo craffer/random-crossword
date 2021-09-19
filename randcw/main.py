caae0e2f5045acafaaab1e33a759aa7cc8592ce8
Conor Rafferty <craffer@umich.edu>
2021-09-19 14:21:35 -0700

    Add pydoc comments

--- randcw/main.py
+++ randcw/main.py
@@ -1,3 +1,4 @@
+"""Generate and output a random link to a puzzle from the NYT archive."""
 import random
 import datetime
 
@@ -16,12 +17,18 @@ WEEKDAYS = [
 
 
 def random_date_helper(start_date, end_date):
+    """Randomly generate a date in between two others."""
     delta = end_date - start_date
     random_day = random.randrange(delta.days)
     return start_date + datetime.timedelta(days=random_day)
 
 
 def random_date(start_date, end_date, weekday=None):
+    """
+    Randomly generate a date in between two others.
+
+    Optionally, ensure that that date falls on a specific weekday.
+    """
     while True:
         date = random_date_helper(start_date, end_date)
         # generate a new date if until we get the one we're looking for if one is specified
@@ -30,10 +37,12 @@ def random_date(start_date, end_date, weekday=None):
 
 
 def date_to_nyt_url(date):
+    """Return a string formatted to link to a crossword puzzle from the NYT Archives."""
     return f"https://www.nytimes.com/crosswords/game/daily/{date.year}/{date.month}/{date.day}"
 
 
 def output_options():
+    """Output numbered options for days of the week."""
     print("Select a weekday:\n")
     for i, day in enumerate(WEEKDAYS):
         print(f"{i}: {day}")
@@ -41,11 +50,13 @@ def output_options():
 
 
 def output_puzzle(date):
+    """Given a date, output a link to a puzzle from the NYT archive."""
     print(f"\nGenerated a link to a random {WEEKDAYS[date.weekday()]} puzzle:")
     print(date_to_nyt_url(date))
 
 
 def run_generator():
+    """Generate and output a random link to a puzzle from the NYT archive."""
     puzzle_date = None
     while not puzzle_date:
         output_options()
@@ -64,6 +75,7 @@ def run_generator():
 
 
 def main():
+    """Run the random generator."""
     run_generator()
 
 
